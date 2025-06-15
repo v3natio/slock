c0b0f81e248ff68d4a1769659f583395ebf99afb
v3natio <72844565+v3natio@users.noreply.github.com>
2025-06-14 19:49:14 -0500

    added xresources patch

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple X Locker ![slock](https://img.shields.io/static/v1?label=st&message=1.5&color=blue&style=flat-square)
+
+`slock` is the simple screen locker utility for X.
+
+## Upstream
+
+The upstream development for `slock` can be found [here](https://git.suckless.org/slock).
+
+# Patches
+
+- [xresources](https://tools.suckless.org/slock/patches/xresources/)
+
+# Install
+
+Download the source code from this repository:
+
+```
+git clone https://github.com/v3natio/slock.git
+cd slock
+sudo make clean install
+```
--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "venatio";
+static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] = "black",       /* after initialization */
@@ -15,7 +15,7 @@ static const char *colorname[NUMCOLS] = {
 ResourcePref resources[] = {
   { "color0",       STRING,  &colorname[INIT] },
   { "color4",       STRING,  &colorname[INPUT] },
-  { "color1",       STRING,  &colorname[FAILED] },
+  { "color9",       STRING,  &colorname[FAILED] },
   //{ "color3",       STRING,  &colorname[CAPS] },
 };
 
